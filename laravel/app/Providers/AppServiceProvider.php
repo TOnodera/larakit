ba3b3d55f3e2b6ec2ce9e59461b94a0201ac05ae
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-18 19:25:00 +0900

    passport,laravel/ui --authの設定した

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
     /**
      * Register any application services.
      *
@@ -24,5 +36,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $this->registerPolicies();
+
+        Passport::routes();
     }
 }
