243a9fcfa6db63560b56c48aa145db5de76c70af
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-16 00:02:42 +0900

    laravel起動確認

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/vue', function () {
-    return view('vue');
+    return view('welcome');
 });
