dbfaff9ac85b374e4868c399d07d52ed80db74fe
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-17 14:07:16 +0900

    sanctum動作確認完了

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
-
-Route::get('/vue', function () {
-    return view('vue');
 });
