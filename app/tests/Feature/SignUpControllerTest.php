98e450d9f903985763e477031c3af5c2f5752f37
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-06-30 19:47:07 +0900

    サインアップコントローラとテスト作成

---
+++ app/tests/Feature/SignUpControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+* @see App\Http\Controllers\SignUpController;
+*/
+class SignUpControllerTest extends TestCase
+{
+    /**
+     * @test
+     * index
+     */
+    public function ユーザー登録画面を開ける()
+    {
+        $this->get('signup')
+            ->assertOk();
+    }
+}
