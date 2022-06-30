4399d645f377caac43e28cd148d005d536533109
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-01 05:02:43 +0900

    実装中

--- app/tests/Feature/SignUpControllerTest.php
+++ app/tests/Feature/SignUpControllerTest.php
@@ -1,8 +1,10 @@
 <?php
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 /**
@@ -10,13 +12,41 @@ use Tests\TestCase;
 */
 class SignUpControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      * index
      */
     public function ユーザー登録画面を開ける()
     {
+        $this->withoutExceptionHandling();
         $this->get('signup')
             ->assertOk();
+    }
+
+    /**
+     * @test store
+     */
+    public function ユーザー登録できる()
+    {
+        // データ検証
+        // DBに保存
+        // ログインさせてからマイページにリダイレクト
+        $validData = [
+            'name' => '太郎',
+            'email' => 'aaa@bbb.ccc',
+            'password' => 'P@ssw0rd'
+        ];
+
+        $this->post('signup', $validData)
+            ->assertOk();
+
+        unset($validData['password']);
+        $this->assertDatabaseHas('users', $validData);
+
+        // パスワードの検証
+        $user = User::firstWhere($validData);
+        $this->assertTrue(Hash::check('P@ssw0rd', $user->password));
     }
 }
